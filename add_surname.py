b844db93df154cc0710729e6f59e5a41ce1be164
ParraL1 <97068405+ParraL1@users.noreply.github.com>
2022-02-09 14:09:41 -0600

    Initial commit

---
+++ add_surname.py
@@ -0,0 +1,10 @@
+#Name: Lilliana Parra
+#Github user: ParraL1
+#Date: 2/9/2022
+#Description: Adds last name to a list of first names
+
+
+
+def add_surname(name_list):
+   Full_Name = [names+' Kardashian' for names in name_list if names[0]=='K']
+   return Full_Name
